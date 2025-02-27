5482591d164294ee364b66ccd9597ad1ef529118
Maksoto1210 <maximkolev07@gmail.com>
2025-02-27 18:50:17 +0200

    Add files via upload

---
+++ zadachkazaplati/zadachkazaplati/Company.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadachkazaplati
+{
+    public class Company
+    {
+        public List<Employee> Employees { get; set; }
+
+        public Company()
+        {
+            Employees = new List<Employee>();
+        }
+
+        public string FindEmployeeBySalary(int salary)
+        {
+            foreach (var employee in Employees)
+            {
+                if (employee.Salary == salary)
+                {
+                    return employee.Name;
+                }
+            }
+            return "Не е намерен";
+        }
+    }
+}
---
+++ zadachkazaplati/zadachkazaplati/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace zadachkazaplati
+{
+    internal class Program
+    {
+
+
+        public static void Main()
+        {
+            Company company = new Company();
+            company.Employees.Add(new Employee("Даниел", 6969));
+            company.Employees.Add(new Employee("Мартин", 5000));
+            company.Employees.Add(new Employee("Анастас", 3000));
+            company.Employees.Add(new Employee("Максим", 7000));
+
+            string result = company.FindEmployeeBySalary(7000);
+            Console.WriteLine(result);
+        }
+
+    }
+}
+    
+
