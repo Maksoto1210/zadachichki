589338485f13433194f8198bb4252c6627806a26
Maksoto1210 <maximkolev07@gmail.com>
2025-03-06 18:21:00 +0200

    Add files via upload

---
+++ zadachka1/zadachka1/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadachka1
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+        public Node Prev;
+
+        public Node(int data) {
+            Data = data;
+            Next = null;
+            Prev = null;
+        }
+    }
+}
---
+++ zadachka1/zadachka1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadachka1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DoublyLinkedList list=new DoublyLinkedList(); ;
+            list.AddToFront(10);
+            list.AddToFront(20);
+            list.AddToFront(30);
+            list.AddToFront(40);
+
+            Console.WriteLine("Original list: ");
+            list.PrintList();
+
+            list.Reverse();
+
+            Console.WriteLine("Reversed list: ");
+            list.PrintList();
+
+        }
+    }
+}
